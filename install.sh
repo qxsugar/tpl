f709b9bfac26ad0453bfd2b2c56c446edba7425d
sugar <qxsugar@gmail.com>
2022-05-31 18:44:49 +0800

    fix install sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ PROJECT_DIR=.tmp/
 rm -rf $PROJECT_DIR
 mkdir -p $PROJECT_DIR
 git clone git@github.com:qxsugar/tpl.git $PROJECT_DIR
-go-archetype transform --transformations="${PROJECT_DIR}transformation-api.yml" --source="${PROJECT_DIR}src/api" --destination=${dest}
+go-archetype transform --transformations=${PROJECT_DIR}transformation-api.yml --source=${PROJECT_DIR}src/api --destination=${dest}
 rm -rf $PROJECT_DIR
 echo "done"
