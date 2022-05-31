53a1e2686954a34344f2de92116cd4c8ba507bee
sugar <qxsugar@gmail.com>
2022-05-31 18:53:48 +0800

    fix install sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ dest="${1:-app}"
 PROJECT_DIR=.tmp/
 rm -rf $PROJECT_DIR
 mkdir -p $PROJECT_DIR
+go get -u github.com/rantav/go-archetype
 git clone git@github.com:qxsugar/tpl.git $PROJECT_DIR
 go-archetype transform --transformations=${PROJECT_DIR}transformation-api.yml --source=${PROJECT_DIR}src/api --destination=${dest}
 rm -rf $PROJECT_DIR
